168c453debae1c234a4cee45d163fe6f753fe953
dbane744 <danielbane@hotmail.co.uk>
2018-08-28 00:26:41 +0100

    Added moving box

---
+++ Scenes/KinematicBody2D.gd
@@ -0,0 +1,38 @@
+extends KinematicBody2D
+
+export (int) var speed = 200
+export (float) var rotation_speed = 1.5
+
+var velocity = Vector2()
+var rotation_dir = 0
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+	
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+
+
+func get_input():
+	rotation_dir = 0
+	velocity = Vector2()
+	if Input.is_action_pressed('right'):
+		rotation_dir += 1
+	if Input.is_action_pressed('left'):
+		rotation_dir -= 1
+	if Input.is_action_pressed('down'):
+		velocity = Vector2(-speed, 0).rotated(rotation)
+	if Input.is_action_pressed('up'):
+		velocity = Vector2(speed, 0).rotated(rotation)
+		
+	
+func _physics_process(delta):
+	get_input()
+	rotation += rotation_dir * rotation_speed * delta
+	move_and_slide(velocity)
+	
