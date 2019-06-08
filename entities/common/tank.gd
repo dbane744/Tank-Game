2414e54a9170136fb1d23768c179c1fe3cd64e4c
dbane744 <danielbane@hotmail.co.uk>
2019-06-08 20:44:31 +0100

    Added animated health pickups for player

--- entities/common/tank.gd
+++ entities/common/tank.gd
@@ -55,6 +55,11 @@ func take_damage(amount):
 	if health <= 0:
 		explode()
 	
+func heal(amount):
+	health += amount
+	health = clamp(health, 0, max_health)
+	emit_signal('health_changed', health * 100/max_health)
+	
 	
 func explode():
 	$CollisionShape2D.disabled = true
